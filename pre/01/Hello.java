22f5323301daae3812743d5a0ce590a4bdf6455b
William <67127399+thunder-red-star@users.noreply.github.com>
2021-09-29 16:25:36 +0000

    work done on replit because easier than nano

--- pre/01/Hello.java
+++ pre/01/Hello.java
@@ -1,5 +1,5 @@
 public class Hello{
-      public static void main(String[] args){
-          System.out.println("Hello world!");
-      }
+    public static void main(String[] args){
+        System.out.println("Hello world!");
+    }
 }
