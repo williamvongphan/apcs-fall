b8ca76096995ab44d2b1562c5bd119cb2e40ae29
William <67127399+thunder-red-star@users.noreply.github.com>
2021-10-27 02:36:52 +0000

    when git push comes after git commit

---
+++ 25_fence/Fence.java
@@ -0,0 +1,14 @@
+public class Fence {
+	public static String fence(int itr) {
+		String res = new String();
+		res = "|";
+		
+		if (itr = 1) {
+			return res;
+		}
+		else {
+			res += fence(itr - 1);
+			return 
+		}
+	}
+}
