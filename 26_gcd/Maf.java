7f71a62648644f86ade6929b8925cebdd2a991c3
William <67127399+thunder-red-star@users.noreply.github.com>
2021-10-27 16:21:45 +0000

    aaaaaaaaa

--- 26_gcd/Maf.java
+++ 26_gcd/Maf.java
@@ -20,5 +20,8 @@ public class Maf {
 
 	public static void main (String[] args) {
 		System.out.println("The GCD of 15 and 6 is " + GCD(15, 6));
+		System.out.println("The GCD of 15 and 6 is " + GCD(1874, 192));
+		System.out.println("The GCD of 15 and 6 is " + GCD(15.0, 6.0));
+		System.out.println("The GCD of 15 and 6 is " + GCD(1874/0, 192.0));
 	}
 }
