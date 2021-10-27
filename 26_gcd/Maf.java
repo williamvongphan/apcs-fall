4f3debf919e16864e79cb008b59ba35b0e5c6891
William <67127399+thunder-red-star@users.noreply.github.com>
2021-10-27 16:25:29 +0000

    when you add comments

--- 26_gcd/Maf.java
+++ 26_gcd/Maf.java
@@ -5,19 +5,19 @@ public class Maf {
 		}
 		return a;
 	}
-
+	// added more me-fods
 	public static long GCD (long a, long b) {
 		return (long) GCD((double) a, (double) b);
 	}
-
+	// added more me-fods
 	public static int GCD (int a, int b) {
 		return (int) GCD((double) a, (double) b);
 	}
-
+	// added more me-fods
 	public static float GCD (float a, float b) {
 		return (float) GCD((double) a, (double) b);
 	}
-
+	// added more me-fods
 	public static void main (String[] args) {
 		System.out.println("The GCD of 15 and 6 is " + GCD(15, 6));
 		System.out.println("The GCD of 1874 and 192 is " + GCD(1874, 192));
