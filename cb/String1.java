8e3b76516cacd5603a2d8eab65e434670592133e
William <67127399+thunder-red-star@users.noreply.github.com>
2021-11-01 11:53:55 -0400

    whoops

---
+++ cb/String1.java
@@ -0,0 +1,8 @@
+public class Answers {
+	public static String helloName(String name) {
+  		return "Hello " + name + "!";
+	}
+	public static String makeAbba(String a, String b) {
+  		return a + b + b + a;
+	}
+}
