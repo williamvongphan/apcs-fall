e53f13f5fe72fb1527126acd7e17d27ee9c19b19
William <67127399+thunder-red-star@users.noreply.github.com>
2021-11-02 18:39:50 +0000

    vscode is a joke.

---
+++ lols/after_vscode.java
@@ -0,0 +1,508 @@
+import static java.lang.System.out;
+
+public class vscode {
+    private static final String WHAT_I_ACTUALLY_WANT_TO_CODE100 = "What I actually want to code";
+    private static final String WHAT_I_ACTUALLY_WANT_TO_CODE99 = WHAT_I_ACTUALLY_WANT_TO_CODE100;
+    private static final String WHAT_I_ACTUALLY_WANT_TO_CODE98 = WHAT_I_ACTUALLY_WANT_TO_CODE99;
+    private static final String WHAT_I_ACTUALLY_WANT_TO_CODE97 = WHAT_I_ACTUALLY_WANT_TO_CODE98;
+    private static final String WHAT_I_ACTUALLY_WANT_TO_CODE96 = WHAT_I_ACTUALLY_WANT_TO_CODE97;
+    private static final String WHAT_I_ACTUALLY_WANT_TO_CODE95 = WHAT_I_ACTUALLY_WANT_TO_CODE96;
+    private static final String WHAT_I_ACTUALLY_WANT_TO_CODE94 = WHAT_I_ACTUALLY_WANT_TO_CODE95;
+    private static final String WHAT_I_ACTUALLY_WANT_TO_CODE93 = WHAT_I_ACTUALLY_WANT_TO_CODE94;
+    private static final String WHAT_I_ACTUALLY_WANT_TO_CODE92 = WHAT_I_ACTUALLY_WANT_TO_CODE93;
+    private static final String WHAT_I_ACTUALLY_WANT_TO_CODE91 = WHAT_I_ACTUALLY_WANT_TO_CODE92;
+    private static final String WHAT_I_ACTUALLY_WANT_TO_CODE90 = WHAT_I_ACTUALLY_WANT_TO_CODE91;
+    private static final String WHAT_I_ACTUALLY_WANT_TO_CODE89 = WHAT_I_ACTUALLY_WANT_TO_CODE90;
+    private static final String WHAT_I_ACTUALLY_WANT_TO_CODE88 = WHAT_I_ACTUALLY_WANT_TO_CODE89;
+    private static final String WHAT_I_ACTUALLY_WANT_TO_CODE87 = WHAT_I_ACTUALLY_WANT_TO_CODE88;
+    private static final String WHAT_I_ACTUALLY_WANT_TO_CODE86 = WHAT_I_ACTUALLY_WANT_TO_CODE87;
+    private static final String WHAT_I_ACTUALLY_WANT_TO_CODE85 = WHAT_I_ACTUALLY_WANT_TO_CODE86;
+    private static final String WHAT_I_ACTUALLY_WANT_TO_CODE84 = WHAT_I_ACTUALLY_WANT_TO_CODE85;
+    private static final String WHAT_I_ACTUALLY_WANT_TO_CODE83 = WHAT_I_ACTUALLY_WANT_TO_CODE84;
+    private static final String WHAT_I_ACTUALLY_WANT_TO_CODE82 = WHAT_I_ACTUALLY_WANT_TO_CODE83;
+    private static final String WHAT_I_ACTUALLY_WANT_TO_CODE81 = WHAT_I_ACTUALLY_WANT_TO_CODE82;
+    private static final String WHAT_I_ACTUALLY_WANT_TO_CODE80 = WHAT_I_ACTUALLY_WANT_TO_CODE81;
+    private static final String WHAT_I_ACTUALLY_WANT_TO_CODE79 = WHAT_I_ACTUALLY_WANT_TO_CODE80;
+    private static final String WHAT_I_ACTUALLY_WANT_TO_CODE78 = WHAT_I_ACTUALLY_WANT_TO_CODE79;
+    private static final String WHAT_I_ACTUALLY_WANT_TO_CODE77 = WHAT_I_ACTUALLY_WANT_TO_CODE78;
+    private static final String WHAT_I_ACTUALLY_WANT_TO_CODE76 = WHAT_I_ACTUALLY_WANT_TO_CODE77;
+    private static final String WHAT_I_ACTUALLY_WANT_TO_CODE75 = WHAT_I_ACTUALLY_WANT_TO_CODE76;
+    private static final String WHAT_I_ACTUALLY_WANT_TO_CODE74 = WHAT_I_ACTUALLY_WANT_TO_CODE75;
+    private static final String WHAT_I_ACTUALLY_WANT_TO_CODE73 = WHAT_I_ACTUALLY_WANT_TO_CODE74;
+    private static final String WHAT_I_ACTUALLY_WANT_TO_CODE72 = WHAT_I_ACTUALLY_WANT_TO_CODE73;
+    private static final String WHAT_I_ACTUALLY_WANT_TO_CODE71 = WHAT_I_ACTUALLY_WANT_TO_CODE72;
+    private static final String WHAT_I_ACTUALLY_WANT_TO_CODE70 = WHAT_I_ACTUALLY_WANT_TO_CODE71;
+    private static final String WHAT_I_ACTUALLY_WANT_TO_CODE69 = WHAT_I_ACTUALLY_WANT_TO_CODE70;
+    private static final String WHAT_I_ACTUALLY_WANT_TO_CODE68 = WHAT_I_ACTUALLY_WANT_TO_CODE69;
+    private static final String WHAT_I_ACTUALLY_WANT_TO_CODE67 = WHAT_I_ACTUALLY_WANT_TO_CODE68;
+    private static final String WHAT_I_ACTUALLY_WANT_TO_CODE66 = WHAT_I_ACTUALLY_WANT_TO_CODE67;
+    private static final String WHAT_I_ACTUALLY_WANT_TO_CODE65 = WHAT_I_ACTUALLY_WANT_TO_CODE66;
+    private static final String WHAT_I_ACTUALLY_WANT_TO_CODE64 = WHAT_I_ACTUALLY_WANT_TO_CODE65;
+    private static final String WHAT_I_ACTUALLY_WANT_TO_CODE63 = WHAT_I_ACTUALLY_WANT_TO_CODE64;
+    private static final String WHAT_I_ACTUALLY_WANT_TO_CODE62 = WHAT_I_ACTUALLY_WANT_TO_CODE63;
+    private static final String WHAT_I_ACTUALLY_WANT_TO_CODE61 = WHAT_I_ACTUALLY_WANT_TO_CODE62;
+    private static final String WHAT_I_ACTUALLY_WANT_TO_CODE60 = WHAT_I_ACTUALLY_WANT_TO_CODE61;
+    private static final String WHAT_I_ACTUALLY_WANT_TO_CODE59 = WHAT_I_ACTUALLY_WANT_TO_CODE60;
+    private static final String WHAT_I_ACTUALLY_WANT_TO_CODE58 = WHAT_I_ACTUALLY_WANT_TO_CODE59;
+    private static final String WHAT_I_ACTUALLY_WANT_TO_CODE57 = WHAT_I_ACTUALLY_WANT_TO_CODE58;
+    private static final String WHAT_I_ACTUALLY_WANT_TO_CODE56 = WHAT_I_ACTUALLY_WANT_TO_CODE57;
+    private static final String WHAT_I_ACTUALLY_WANT_TO_CODE55 = WHAT_I_ACTUALLY_WANT_TO_CODE56;
+    private static final String WHAT_I_ACTUALLY_WANT_TO_CODE54 = WHAT_I_ACTUALLY_WANT_TO_CODE55;
+    private static final String WHAT_I_ACTUALLY_WANT_TO_CODE53 = WHAT_I_ACTUALLY_WANT_TO_CODE54;
+    private static final String WHAT_I_ACTUALLY_WANT_TO_CODE52 = WHAT_I_ACTUALLY_WANT_TO_CODE53;
+    private static final String WHAT_I_ACTUALLY_WANT_TO_CODE51 = WHAT_I_ACTUALLY_WANT_TO_CODE52;
+    private static final String WHAT_I_ACTUALLY_WANT_TO_CODE50 = WHAT_I_ACTUALLY_WANT_TO_CODE51;
+    private static final String WHAT_I_ACTUALLY_WANT_TO_CODE49 = WHAT_I_ACTUALLY_WANT_TO_CODE50;
+    private static final String WHAT_I_ACTUALLY_WANT_TO_CODE48 = WHAT_I_ACTUALLY_WANT_TO_CODE49;
+    private static final String WHAT_I_ACTUALLY_WANT_TO_CODE47 = WHAT_I_ACTUALLY_WANT_TO_CODE48;
+    private static final String WHAT_I_ACTUALLY_WANT_TO_CODE46 = WHAT_I_ACTUALLY_WANT_TO_CODE47;
+    private static final String WHAT_I_ACTUALLY_WANT_TO_CODE45 = WHAT_I_ACTUALLY_WANT_TO_CODE46;
+    private static final String WHAT_I_ACTUALLY_WANT_TO_CODE44 = WHAT_I_ACTUALLY_WANT_TO_CODE45;
+    private static final String WHAT_I_ACTUALLY_WANT_TO_CODE43 = WHAT_I_ACTUALLY_WANT_TO_CODE44;
+    private static final String WHAT_I_ACTUALLY_WANT_TO_CODE42 = WHAT_I_ACTUALLY_WANT_TO_CODE43;
+    private static final String WHAT_I_ACTUALLY_WANT_TO_CODE41 = WHAT_I_ACTUALLY_WANT_TO_CODE42;
+    private static final String WHAT_I_ACTUALLY_WANT_TO_CODE40 = WHAT_I_ACTUALLY_WANT_TO_CODE41;
+    private static final String WHAT_I_ACTUALLY_WANT_TO_CODE39 = WHAT_I_ACTUALLY_WANT_TO_CODE40;
+    private static final String WHAT_I_ACTUALLY_WANT_TO_CODE38 = WHAT_I_ACTUALLY_WANT_TO_CODE39;
+    private static final String WHAT_I_ACTUALLY_WANT_TO_CODE37 = WHAT_I_ACTUALLY_WANT_TO_CODE38;
+    private static final String WHAT_I_ACTUALLY_WANT_TO_CODE36 = WHAT_I_ACTUALLY_WANT_TO_CODE37;
+    private static final String WHAT_I_ACTUALLY_WANT_TO_CODE35 = WHAT_I_ACTUALLY_WANT_TO_CODE36;
+    private static final String WHAT_I_ACTUALLY_WANT_TO_CODE34 = WHAT_I_ACTUALLY_WANT_TO_CODE35;
+    private static final String WHAT_I_ACTUALLY_WANT_TO_CODE33 = WHAT_I_ACTUALLY_WANT_TO_CODE34;
+    private static final String WHAT_I_ACTUALLY_WANT_TO_CODE32 = WHAT_I_ACTUALLY_WANT_TO_CODE33;
+    private static final String WHAT_I_ACTUALLY_WANT_TO_CODE31 = WHAT_I_ACTUALLY_WANT_TO_CODE32;
+    private static final String WHAT_I_ACTUALLY_WANT_TO_CODE30 = WHAT_I_ACTUALLY_WANT_TO_CODE31;
+    private static final String WHAT_I_ACTUALLY_WANT_TO_CODE29 = WHAT_I_ACTUALLY_WANT_TO_CODE30;
+    private static final String WHAT_I_ACTUALLY_WANT_TO_CODE28 = WHAT_I_ACTUALLY_WANT_TO_CODE29;
+    private static final String WHAT_I_ACTUALLY_WANT_TO_CODE27 = WHAT_I_ACTUALLY_WANT_TO_CODE28;
+    private static final String WHAT_I_ACTUALLY_WANT_TO_CODE26 = WHAT_I_ACTUALLY_WANT_TO_CODE27;
+    private static final String WHAT_I_ACTUALLY_WANT_TO_CODE25 = WHAT_I_ACTUALLY_WANT_TO_CODE26;
+    private static final String WHAT_I_ACTUALLY_WANT_TO_CODE24 = WHAT_I_ACTUALLY_WANT_TO_CODE25;
+    private static final String WHAT_I_ACTUALLY_WANT_TO_CODE23 = WHAT_I_ACTUALLY_WANT_TO_CODE24;
+    private static final String WHAT_I_ACTUALLY_WANT_TO_CODE22 = WHAT_I_ACTUALLY_WANT_TO_CODE23;
+    private static final String WHAT_I_ACTUALLY_WANT_TO_CODE21 = WHAT_I_ACTUALLY_WANT_TO_CODE22;
+    private static final String WHAT_I_ACTUALLY_WANT_TO_CODE20 = WHAT_I_ACTUALLY_WANT_TO_CODE21;
+    private static final String WHAT_I_ACTUALLY_WANT_TO_CODE19 = WHAT_I_ACTUALLY_WANT_TO_CODE20;
+    private static final String WHAT_I_ACTUALLY_WANT_TO_CODE18 = WHAT_I_ACTUALLY_WANT_TO_CODE19;
+    private static final String WHAT_I_ACTUALLY_WANT_TO_CODE17 = WHAT_I_ACTUALLY_WANT_TO_CODE18;
+    private static final String WHAT_I_ACTUALLY_WANT_TO_CODE16 = WHAT_I_ACTUALLY_WANT_TO_CODE17;
+    private static final String WHAT_I_ACTUALLY_WANT_TO_CODE15 = WHAT_I_ACTUALLY_WANT_TO_CODE16;
+    private static final String WHAT_I_ACTUALLY_WANT_TO_CODE14 = WHAT_I_ACTUALLY_WANT_TO_CODE15;
+    private static final String WHAT_I_ACTUALLY_WANT_TO_CODE13 = WHAT_I_ACTUALLY_WANT_TO_CODE14;
+    private static final String WHAT_I_ACTUALLY_WANT_TO_CODE12 = WHAT_I_ACTUALLY_WANT_TO_CODE13;
+    private static final String WHAT_I_ACTUALLY_WANT_TO_CODE11 = WHAT_I_ACTUALLY_WANT_TO_CODE12;
+    private static final String WHAT_I_ACTUALLY_WANT_TO_CODE10 = WHAT_I_ACTUALLY_WANT_TO_CODE11;
+    private static final String WHAT_I_ACTUALLY_WANT_TO_CODE9 = WHAT_I_ACTUALLY_WANT_TO_CODE10;
+    private static final String WHAT_I_ACTUALLY_WANT_TO_CODE8 = WHAT_I_ACTUALLY_WANT_TO_CODE9;
+    private static final String WHAT_I_ACTUALLY_WANT_TO_CODE7 = WHAT_I_ACTUALLY_WANT_TO_CODE8;
+    private static final String WHAT_I_ACTUALLY_WANT_TO_CODE6 = WHAT_I_ACTUALLY_WANT_TO_CODE7;
+    private static final String WHAT_I_ACTUALLY_WANT_TO_CODE5 = WHAT_I_ACTUALLY_WANT_TO_CODE6;
+    private static final String WHAT_I_ACTUALLY_WANT_TO_CODE4 = WHAT_I_ACTUALLY_WANT_TO_CODE5;
+    private static final String WHAT_I_ACTUALLY_WANT_TO_CODE3 = WHAT_I_ACTUALLY_WANT_TO_CODE4;
+    private static final String WHAT_I_ACTUALLY_WANT_TO_CODE2 = WHAT_I_ACTUALLY_WANT_TO_CODE3;
+    private static final String WHAT_I_ACTUALLY_WANT_TO_CODE = WHAT_I_ACTUALLY_WANT_TO_CODE2;
+    
+    public static void main (String[] args) {
+        extracted();
+    }
+
+    private static void extracted() {
+        extracted2();
+    }
+
+    public static void extracted2() {
+        extracted2();
+    }
+    
+    public static void extracted3() {
+        extracted3();
+    }
+    
+    public static void extracted4() {
+        extracted4();
+    }
+    
+    public static void extracted5() {
+        extracted5();
+    }
+    
+    public static void extracted6() {
+        extracted6();
+    }
+    
+    public static void extracted7() {
+        extracted7();
+    }
+    
+    public static void extracted8() {
+        extracted8();
+    }
+    
+    public static void extracted9() {
+        extracted9();
+    }
+    
+    public static void extracted10() {
+        extracted10();
+    }
+    
+    public static void extracted11() {
+        extracted11();
+    }
+    
+    public static void extracted12() {
+        extracted12();
+    }
+    
+    public static void extracted13() {
+        extracted13();
+    }
+    
+    public static void extracted14() {
+        extracted14();
+    }
+    
+    public static void extracted15() {
+        extracted15();
+    }
+    
+    public static void extracted16() {
+        extracted16();
+    }
+    
+    public static void extracted17() {
+        extracted17();
+    }
+    
+    public static void extracted18() {
+        extracted18();
+    }
+    
+    public static void extracted19() {
+        extracted19();
+    }
+    
+    public static void extracted20() {
+        extracted20();
+    }
+    
+    public static void extracted21() {
+        extracted21();
+    }
+    
+    public static void extracted22() {
+        extracted22();
+    }
+    
+    public static void extracted23() {
+        extracted23();
+    }
+    
+    public static void extracted24() {
+        extracted24();
+    }
+    
+    public static void extracted25() {
+        extracted25();
+    }
+    
+    public static void extracted26() {
+        extracted26();
+    }
+    
+    public static void extracted27() {
+        extracted27();
+    }
+    
+    public static void extracted28() {
+        extracted28();
+    }
+    
+    public static void extracted29() {
+        extracted29();
+    }
+    
+    public static void extracted30() {
+        extracted30();
+    }
+    
+    public static void extracted31() {
+        extracted31();
+    }
+    
+    public static void extracted32() {
+        extracted32();
+    }
+    
+    public static void extracted33() {
+        extracted33();
+    }
+    
+    public static void extracted34() {
+        extracted34();
+    }
+    
+    public static void extracted35() {
+        extracted35();
+    }
+    
+    public static void extracted36() {
+        extracted36();
+    }
+    
+    public static void extracted37() {
+        extracted37();
+    }
+    
+    public static void extracted38() {
+        extracted38();
+    }
+    
+    public static void extracted39() {
+        extracted39();
+    }
+    
+    public static void extracted40() {
+        extracted40();
+    }
+    
+    public static void extracted41() {
+        extracted41();
+    }
+    
+    public static void extracted42() {
+        extracted42();
+    }
+    
+    public static void extracted43() {
+        extracted43();
+    }
+    
+    public static void extracted44() {
+        extracted44();
+    }
+    
+    public static void extracted45() {
+        extracted45();
+    }
+    
+    public static void extracted46() {
+        extracted46();
+    }
+    
+    public static void extracted47() {
+        extracted47();
+    }
+    
+    public static void extracted48() {
+        extracted48();
+    }
+    
+    public static void extracted49() {
+        extracted49();
+    }
+    
+    public static void extracted50() {
+        extracted50();
+    }
+    
+    public static void extracted51() {
+        extracted51();
+    }
+    
+    public static void extracted52() {
+        extracted52();
+    }
+    
+    public static void extracted53() {
+        extracted53();
+    }
+    
+    public static void extracted54() {
+        extracted54();
+    }
+    
+    public static void extracted55() {
+        extracted55();
+    }
+    
+    public static void extracted56() {
+        extracted56();
+    }
+    
+    public static void extracted57() {
+        extracted57();
+    }
+    
+    public static void extracted58() {
+        extracted58();
+    }
+    
+    public static void extracted59() {
+        extracted59();
+    }
+    
+    public static void extracted60() {
+        extracted60();
+    }
+    
+    public static void extracted61() {
+        extracted61();
+    }
+    
+    public static void extracted62() {
+        extracted62();
+    }
+    
+    public static void extracted63() {
+        extracted63();
+    }
+    
+    public static void extracted64() {
+        extracted64();
+    }
+    
+    public static void extracted65() {
+        extracted65();
+    }
+    
+    public static void extracted66() {
+        extracted66();
+    }
+    
+    public static void extracted67() {
+        extracted67();
+    }
+    
+    public static void extracted68() {
+        extracted68();
+    }
+    
+    public static void extracted69() {
+        extracted69();
+    }
+    
+    public static void extracted70() {
+        extracted70();
+    }
+    
+    public static void extracted71() {
+        extracted71();
+    }
+    
+    public static void extracted72() {
+        extracted72();
+    }
+    
+    public static void extracted73() {
+        extracted73();
+    }
+    
+    public static void extracted74() {
+        extracted74();
+    }
+    
+    public static void extracted75() {
+        extracted75();
+    }
+    
+    public static void extracted76() {
+        extracted76();
+    }
+    
+    public static void extracted77() {
+        extracted77();
+    }
+    
+    public static void extracted78() {
+        extracted78();
+    }
+    
+    public static void extracted79() {
+        extracted79();
+    }
+    
+    public static void extracted80() {
+        extracted80();
+    }
+    
+    public static void extracted81() {
+        extracted81();
+    }
+    
+    public static void extracted82() {
+        extracted82();
+    }
+    
+    public static void extracted83() {
+        extracted83();
+    }
+    
+    public static void extracted84() {
+        extracted84();
+    }
+    
+    public static void extracted85() {
+        extracted85();
+    }
+    
+    public static void extracted86() {
+        extracted86();
+    }
+    
+    public static void extracted87() {
+        extracted87();
+    }
+    
+    public static void extracted88() {
+        extracted88();
+    }
+    
+    public static void extracted89() {
+        extracted89();
+    }
+    
+    public static void extracted90() {
+        extracted90();
+    }
+    
+    public static void extracted91() {
+        extracted91();
+    }
+    
+    public static void extracted92() {
+        extracted92();
+    }
+    
+    public static void extracted93() {
+        extracted93();
+    }
+    
+    public static void extracted94() {
+        extracted94();
+    }
+    
+    public static void extracted95() {
+        extracted95();
+    }
+    
+    public static void extracted96() {
+        extracted96();
+    }
+    
+    public static void extracted97() {
+        extracted97();
+    }
+    
+    public static void extracted98() {
+        extracted98();
+    }
+    
+    public static void extracted99() {
+        extracted100();
+    }
+
+    private static void extracted100() {
+        out.println(WHAT_I_ACTUALLY_WANT_TO_CODE100);
+    }
+}
---
+++ lols/vscode.java
@@ -0,0 +1,5 @@
+public class vscode {
+    public static void main (String[] args) {
+        System.out.println("What I actually want to code");
+    }
+}
