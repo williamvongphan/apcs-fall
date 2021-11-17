5b52767a62d4860bf9ffe1c2d458efef1c4da310
thunder-red-star <wvongphanith30@stuy.edu>
2021-11-17 11:50:03 -0500

    Automated commit by push.sh

---
+++ 38_slots/Slots.java
@@ -0,0 +1,162 @@
+/*****************************************************
+ * Clyde "Thluffy" Sinclair
+ * APCS pd00
+ * HW38 -- Shmoney
+ * 2021-11-18
+ *
+ * class Slots
+ * skeleton
+ *****************************************************/
+
+public class Slots {
+
+	//instance variable to represent master copy for slot machine
+	private static final String[] FRUITS = {
+		"lime",
+		"lime",
+		"lime",
+		"lemon",
+		"lemon",
+		"lemon",
+		"cherry",
+		"cherry",
+		"cherry",
+		/*
+		  add extra fruits until your heart is content...
+		  Some suggestions:
+		"orange", "orange", "orange", 
+		"grapefruit", "grapefruit", "grapefruit", 
+		"tangerine", "tangerine", "tangerine", 
+		"ugli", "ugli", "ugli", 
+		*/
+		"peach",
+		"peach",
+		"peach"
+	};
+
+	private String[] _fruits; //to be init'd by each instance
+
+
+	/*=====================================
+	  Slots() -- default constructor
+	  pre:  constant array FRUITS exists, has been initialized
+	  post: mutable array _fruits contains same elements as FRUITS
+	  =====================================*/
+	public Slots() {
+		//allocate memory for _fruits based on size of FRUITS:
+
+
+		//copy elements of FRUITS into _fruits:
+
+	}
+
+
+	/*=====================================
+	  String toString() -- overrides inherited toString()
+	  pre:  
+	  post: returns String of elements in slots 0 thru 2, separated by tabs
+	  =====================================*/
+	public String toString() {
+
+	}
+
+
+	/*=====================================
+	  void swap(int,int) -- array swap util fxn
+	  pre:  _fruits array exists
+	  post: elements at indices i, j are swapped
+	  =====================================*/
+	private void swap(int i, int j) {
+		String[] temp = _fruits;
+		temp[i] = _fruits[j];
+		temp[j] = _fruits[i];
+	}
+
+
+	/*=====================================
+	  void spinOnce() -- simulate a pull of the slot machine lever
+	  pre:  _fruits array exists
+	  post: randomized order of elements in _fruits array
+	  =====================================*/
+	public void spinOnce() {
+		// A simple approach to shuffling:
+		// iterate through the array, swapping
+		// the val at each index with a randomly chosen other index
+		for ()
+			swap();
+	}
+
+
+	/*=====================================
+	  boolean jackpot() -- checks for a winning combo
+	  pre:  _fruits is existing array
+	  post: returns true if first 3 slots represent winning combo,
+	  false otherwise
+	  =====================================*/
+	public boolean jackpot() {
+		boolean retBoo = false;
+
+
+		return retBoo;
+	}
+
+
+	/*=====================================
+	  boolean miniWin() -- checks for a winning combo
+	  pre:  _fruits is existing array
+	  post: returns true if first 3 slots represent winning combo,
+	  or if first 3 slots mutually distinct, 
+	  false otherwise
+	  =====================================*/
+	public boolean miniWin() {
+		boolean retBoo = ?
+
+
+			return retBoo;
+	}
+
+
+	//main() method for testing
+	public static void main(String[] args) {
+		//usage: move bar below down 1 line at a time to test functionality...
+
+		/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+		Slots machine01 = new Slots();
+		Slots machine02 = new Slots();
+		//test to verify slot machines function indepently
+		System.out.println();
+		System.out.println( "Machine01 initial state:\t" + machine01 );
+		System.out.println( "Machine02 initial state:\t" + machine02 );
+		System.out.println( "\nSpinning machine01...\n" );
+		machine01.spinOnce();
+		System.out.println();
+		System.out.println( "Machine01 state:\t" + machine01 );
+		System.out.println( "Machine02 state:\t" + machine02 );
+		System.out.println();
+		//test gamble-until-you-win mechanism
+		System.out.println( "Preparing to spin until a mini win! . . ." );
+		System.out.println( "------------------------------------" );
+		//if you haven't won, spin again until you win!
+		while( machine01.miniWin() == false ) {
+		  System.out.println( "Your spin..." + "\t" + machine01 );
+		  System.out.println( "LOSE\n" );
+		  machine01.spinOnce();
+		}
+		System.out.println( "====================================" );
+		System.out.println( "Your spin..." + "\t" + machine01 );
+		System.out.println( "WIN\n" );
+		System.out.println( "Preparing to spin until...jackpot! . . ." );
+		System.out.println( "------------------------------------" );
+		//if you haven't won, spin again until you win!
+		while( machine01.jackpot() == false ) {
+		  System.out.println( "Your spin..." + "\t" + machine01 );
+		  System.out.println( "LOSE\n" );
+		  machine01.spinOnce();
+		}
+		System.out.println( "====================================" );
+		System.out.println( "Your spin..." + "\t" + machine01 );
+		System.out.println( "JACKPOT!\n" );
+		  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+	} //end main
+
+} //end class Slots
--- test/test.java
+++ test/test.java
@@ -1,64 +1,6 @@
 public class test {
-    // nCr
-    public static int nCr(int n, int r) {
-        if (n == r) return 1;
-        if (r == 0) return 1;
-        return nCr(n - 1, r - 1) + nCr(n - 1, r);
-    }
+    // Given an array of ints, return true if 6 appears as either the first or last element in the array. The array will be length 1 or more.
 
-    // catalan number
-    public static int catalan(int n) {
-        if (n == 0) return 1;
-        return catalan(n - 1) * (4 * n - 2) / (n + 1);
-    }
 
-    // take the integral of a function
-    public static double integral(double a, double b, double h, Function f) {
-        double sum = 0;
-        for (double i = a; i < b; i += h) {
-            sum += f.f(i) * h;
-        }
-        return sum;
-    }
-
-    // guess someone's name from their height and weight
-    public static String guessName(double height, double weight) {
-        if (height < 150) {
-            if (weight < 50) return "Tiny";
-            if (weight < 100) return "Little";
-            if (weight < 150) return "Small";
-            if (weight < 200) return "Average";
-            if (weight < 250) return "Large";
-            if (weight < 300) return "Big";
-            if (weight < 350) return "Huge";
-            if (weight < 400) return "Giant";
-            return "Gigantic";
-        } else {
-            if (weight < 50) return "Tiny";
-            if (weight < 100) return "Little";
-            if (weight < 150) return "Small";
-            if (weight < 200) return "Average";
-            if (weight < 250) return "Large";
-            if (weight < 300) return "Big";
-            if (weight < 350) return "Huge";
-            if (weight < 400) return "Giant";
-            return "Gigantic";
-        }
-    }
-
-    // return google search results for a query
-    public static String googleSearch(String query) {
-        return "https://www.google.com/search?q=" + query;
-    }
-
-    // make a http request to google and return top result for query
-    public static String googleSearchTopResult(String query) {
-        try {
-            Document doc = Jsoup.connect("https://www.google.com/search?q=" + query).get();
-            Element first = doc.select("div#search").select("div.g").first();
-            return first.select("h3").text();
-        } catch (Exception e) {
-            return "";
-        }
-    }
+    // return the number of primes between a number and another number
 }
